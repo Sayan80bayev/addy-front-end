a4a95f63eabf55971edb4539b6dd45c062c75ca7
Sayan80bayev <sayan123serv@gmail.com>
2024-06-22 22:50:31 +0500

    test

--- src/store/advertismentApi.js
+++ src/store/advertismentApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-//commit
+//commits
 export const advertisementApi = createApi({
   reducerPath: "advertisementApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
