101b18eacb26d354b9eab821bcf7adad7889f107
Sayan80bayev <sayan123serv@gmail.com>
2024-06-22 22:11:10 +0500

    advertismentpApi adding

--- components/adds/AdvertisementList.js
+++ components/adds/AdvertisementList.js
@@ -4,20 +4,22 @@ import LoadingIcon from "../LoadingIcon";
 import { fetchAdvertisements, fetchAdvertisementsWithSort } from "../api"; // предполагается, что fetchAdvertisements принимает параметр сортировки
 import Adds from "./Adds";
 import Sorting from "../Sorting";
-
+import { useGetAddsQuery } from "../../store/advertismentApi";
 function AdvertisementList() {
-  const [advertisements, setAdvertisements] = useState([]);
+  // const [advertisements, setAdvertisements] = useState([]);
   const [loading, setLoading] = useState(false);
   const [sortCriteria, setSortCriteria] = useState(null); // default sorting by date
+  // const [getAdds, { isError }] = useGetAddsQuery();
+  const { advertisements = [] } = useGetAddsQuery();
 
   useEffect(() => {
     if (sortCriteria !== null) {
       const fetchData = async (criteria) => {
-        setAdvertisements([]);
+        // setAdvertisements([]);
         setLoading(true);
         try {
-          const data = await fetchAdvertisementsWithSort(criteria);
-          setAdvertisements(data);
+          // const data = await fetchAdvertisementsWithSort(criteria);
+          // setAdvertisements(data);
         } catch (error) {
           console.error(error);
         } finally {
@@ -25,20 +27,21 @@ function AdvertisementList() {
         }
       };
       fetchData(sortCriteria);
-    } else {
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const data = await fetchAdvertisements();
-          setAdvertisements(data);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchData();
     }
+    // else {
+    //   const fetchData = async () => {
+    //     setLoading(true);
+    //     try {
+    //       const data = await getAdds().unwrap();
+    //       setAdvertisements(data);
+    //     } catch (error) {
+    //       console.error(error);
+    //     } finally {
+    //       setLoading(false);
+    //     }
+    //   };
+    //   fetchData();
+    // }
   }, [sortCriteria]);
 
   const handleSort = (criteria) => {
---
+++ store/advertismentApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const advertisementApi = createApi({
+  reducerPath: "advertisementApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+  tagTypes: ["Advertisements"],
+  endpoints: (build) => ({
+    getAdds: build.query({
+      query: () => "api/v1/public/getAdds",
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Advertisements", id })),
+              { type: "Advertisements", id: "LIST" },
+            ]
+          : [{ type: "Advertisements", id: "LIST" }],
+    }),
+  }),
+});
+
+export const { useGetAddsQuery } = advertisementApi;
